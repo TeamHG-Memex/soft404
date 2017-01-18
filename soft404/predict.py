a2861f4d4e3bf692a1cf0e447f6b91031f32aaf2
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-01-18 18:57:34 +0300

    Allow passing prior probability of a soft404 page

--- soft404/predict.py
+++ soft404/predict.py
@@ -1,4 +1,5 @@
 from functools import partial
+import math
 import os.path
 
 from sklearn.externals import joblib
@@ -10,26 +11,47 @@ default_classifier = None
 
 
 class Soft404Classifier(object):
-    def __init__(self, filename=default_location):
-        self.pipeline = joblib.load(filename)
+    default_soft404_prior = 0.0034  # probability of a link giving a soft404
 
-    def predict(self, html):
-        """ Return probability of the page being a 404 page.
+    def __init__(self, filename=default_location, soft404_prior=None):
+        """ Load trained classifier.
+        soft404_prior is a prior probability of page being a soft404.
+        Default value of 0.0034 was estimated from a crawl that was used to get
+        training data (check notebooks/real_soft404.ipynb).
+        Probability of a link giving a (hard) 404 is about 0.022,
+        and conditional probability of link being a soft 404 if we know it's 404
+        is about 0.15.
         """
-        return float(self.pipeline.predict_proba([html])[0, 1])
+        self.pipeline = joblib.load(filename)
+        self.soft404_prior = soft404_prior or self.default_soft404_prior
+
+    def predict(self, html, soft404_prior=None):
+        """ Return probability of the page being a 404 page.
+        soft404_prior overrides classifier soft404_prior.
+        """
+        score = float(self.pipeline.decision_function([html])[0])
+        # Now re-calibrate score with new bias given soft404_prior
+        soft404_prior = soft404_prior or self.soft404_prior
+        _, clf = self.pipeline.steps[-1]
+        train_bias = clf.intercept_[0]
+        new_bias = _inverse_sigmoid(soft404_prior)
+        return _sigmoid(score - train_bias + new_bias)
 
     @classmethod
     def save_model(cls, filename, pipeline):
         joblib.dump(pipeline, filename, protocol=2, compress=3)
 
 
-def probability(html):
+def probability(html, soft404_prior=None):
     """ Return probability of the page being a 404 page.
+    soft404_prior is a prior probability of page being a soft404.
+    Default value was estimated from a crawl that was used to get
+    training data (check notebooks/real_soft404.ipynb).
     """
     global default_classifier
     if default_classifier is None:
         default_classifier = Soft404Classifier()
-    return default_classifier.predict(html)
+    return default_classifier.predict(html, soft404_prior=soft404_prior)
 
 
 def _function_transformer(fn):
@@ -40,3 +62,11 @@ def _transformer(xs, ys=None, fn=None):
     assert ys is None
     assert fn is not None
     return [fn(x) for x in xs]
+
+
+def _sigmoid(x):
+    return 1 / ( 1 + math.exp(-x))
+
+
+def _inverse_sigmoid(x):
+    return -math.log(1/x - 1)
--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,12 +1,35 @@
 from soft404 import Soft404Classifier, probability
+from soft404.predict import _sigmoid, _inverse_sigmoid
 
 
 def test_predict_classifier():
-    clf = Soft404Classifier()
+    clf = Soft404Classifier(soft404_prior=0.2)
     assert clf.predict('<h1>page not found: 404 error</h1>') > 0.9
     assert clf.predict('<h1>hi here!</h1> just a page') < 0.5
 
 
 def test_predict_function():
-    assert probability('<h1>page not found, oops</h1>') > 0.9
-    assert probability('<h1>hi here</h1> nice to see you') < 0.3
+    assert probability(
+        '<h1>page not found, oops</h1>', soft404_prior=0.2) > 0.9
+    assert probability(
+        '<h1>hi here</h1> nice to see you', soft404_prior=0.2) < 0.3
+
+
+def test_prior():
+    clf = Soft404Classifier()
+    html = '<h1>page not found: 404 error</h1>'
+    assert clf.predict(html) < clf.predict(html, soft404_prior=0.5)
+    assert clf.predict(html) == probability(html)
+    assert (clf.predict(html, soft404_prior=0.2) ==
+            probability(html, soft404_prior=0.2))
+
+    clf = Soft404Classifier(soft404_prior=0.2)
+    html = '<h1>page not found: 404 error</h1>'
+    assert clf.predict(html) > clf.predict(html, soft404_prior=0.1)
+
+    assert probability(html) < probability(html, soft404_prior=0.5)
+
+
+def test_sigmoid():
+    x = -0.905
+    assert abs(_inverse_sigmoid(_sigmoid(x)) - x) < 1e-12
